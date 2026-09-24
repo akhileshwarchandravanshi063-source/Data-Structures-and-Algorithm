3b43e52d4339f38dc2b475b9b335964433b1ea99
akhileshwarchandravanshi063 <akhileshwarchandravanshi063@gmail.com>
2026-09-24 16:38:41 +0530

    Kadanes Algorithm

--- Arrays/Linear Scan/KadanesAlgorithm.java
+++ Arrays/Linear Scan/KadanesAlgorithm.java
@@ -1,6 +1,22 @@
 class KadanesAlgorithm{
     public static void main (String[] agr){
-        
+        int [] arr = {-2,1,-3,4,-1,2,1,-5,4};
+        int maxSum = kadane(arr);
+        System.out.println(maxSum);
 
+    }
+
+    public static int kadane(int[] arr){
+        int n = arr.length;
+        int maxSum = 0;
+        int currentSum = 0;
+        for (int i = 0; i < n; i++) {
+            currentSum += arr[i];
+            maxSum = Math.max(maxSum, currentSum);
+            if (currentSum < 0) {
+                currentSum = 0;
+            }
+        }
+        return maxSum;
     }
 }
